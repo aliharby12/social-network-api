f4bdf7e56cdc2eed7e3a35d8dee1c7095254eee3
aliharby12 <aliharby565@gmail.com>
2022-12-29 12:41:56 +0200

    config jwt, user login and refresh token

--- social/urls.py
+++ social/urls.py
@@ -14,8 +14,48 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from rest_framework import permissions
+from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("user/", include("user.urls")),
 ]
+
+if settings.DEBUG:  # pragma: no cover
+
+    schema_view = get_schema_view(
+        openapi.Info(
+            title="Social API",
+            default_version="1.2",
+            description="Social API",
+        ),
+        public=True,
+        permission_classes=[permissions.AllowAny],
+    )
+
+    urlpatterns = (
+        [
+            # Swagger
+            path(
+                "swagger",
+                schema_view.with_ui("swagger", cache_timeout=0),
+                name="schema-swagger-ui",
+            ),
+            path(
+                "redoc/",
+                schema_view.with_ui("redoc", cache_timeout=0),
+                name="schema-redoc",
+            ),
+        ]
+        + urlpatterns
+        + static(
+            settings.MEDIA_URL,
+            document_root=settings.MEDIA_ROOT,
+        )
+    )
