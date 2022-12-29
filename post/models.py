230aec67c3ec77d5e49e7379a9f18007022f3ec1
aliharby12 <aliharby565@gmail.com>
2022-12-29 12:06:27 +0200

    create database models

--- post/models.py
+++ post/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+
+
+class TimeStampedModel(models.Model):
+    """
+    database model for created at and updated at fields
+    """
+
+    created_at = models.DateTimeField(db_index=True, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Post(TimeStampedModel):
+    """
+    database table to save user posts
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_posts")
+    title = models.CharField(max_length=256)
+    body = models.TextField()
+
+    def get_likes_count(self) -> int:
+        return UserLikedPost.objects.filter(post=self).select_related("post").count()
+
+    def __str__(self) -> str:
+        return f"{self.user.email} : {self.title}"
+
+
+class UserLikedPost(TimeStampedModel):
+    """
+    database table to save user liked posts
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_likes")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="liked_posts")
+
+    def __str__(self) -> str:
+        return f"{self.user.email} liked {self.post.title}"
--- user/models.py
+++ user/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new User"""
+        if not email:
+            raise ValueError("Users must have email address")
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class Gender_Types(models.TextChoices):
+    MALE = "M", "M"
+    FEMALE = "F", "F"
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    joined_at = models.DateTimeField(auto_now_add=True)
+    gender = models.CharField(
+        max_length=5, choices=Gender_Types.choices, null=True, blank=True
+    )
+    signed_up_holiday = models.CharField(max_length=256, blank=True, null=True)
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                name="%(app_label)s_%(class)s_gender_valid",
+                check=models.Q(gender__in=Gender_Types.values),
+            ),
+        ]
+
+    def __str__(self) -> str:
+        return self.email
