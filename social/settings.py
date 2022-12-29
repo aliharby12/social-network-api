3bd258d9c00550d26fd4457731c62b756c3e6f86
aliharby12 <aliharby565@gmail.com>
2022-12-29 11:32:49 +0200

    create user and post apps

--- social/settings.py
+++ social/settings.py
@@ -14,16 +14,18 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+import os, environ
 
-
+environ.Env.read_env()
+env = environ.Env()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0v(^_escd*n6kr1zld2bz96mli3*k!p@6^(14@sh)9thssfk-@"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
